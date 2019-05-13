866d5e04f160f3bdf4b082102942148e4d433165
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-05-13 16:18:25 +0900

    Add article model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  get 'welcome/index'
+
+  resources :articles
+
+  # root to: "home#index"
+  root 'welcome#index'
 end
