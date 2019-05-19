9248b722f087e31cf569af4eda1b827948fe5c95
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-05-19 10:53:17 +0900

    Implement create method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def update
