fb86d4506eae26f53080a0bb003c2462eec854bb
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-05-22 12:24:11 +0900

    Add user model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_004133) do
+ActiveRecord::Schema.define(version: 2019_05_22_032053) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
