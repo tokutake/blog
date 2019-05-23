0f64a224bb8c99a007ec0add65ff45346cf79ecc
Hokuto Tokutake <tokutake.hokuto@gmail.com>
2019-05-23 09:52:02 +0900

    Add email index.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_032053) do
+ActiveRecord::Schema.define(version: 2019_05_22_062501) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_032053) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
